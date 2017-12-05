b0d6ff469d256e84c95164070ac40abe40567a60
CleitonJB <cleitonbraga56@gmail.com>
2017-12-05 20:02:32 -0200

    Atualizando o projeto no NetBeans, foi adicionado os atributos

--- Central_gamers/src/main/java/com/mycompany/centralgamers/usuarios/forum.java
+++ Central_gamers/src/main/java/com/mycompany/centralgamers/usuarios/forum.java
@@ -10,5 +10,79 @@ package com.mycompany.centralgamers.usuarios;
  * @author aluno
  */
 public class forum {
-    
+
+    /**
+     * @return the cod_forum
+     */
+    public int getCod_forum() {
+        return cod_forum;
+    }
+
+    /**
+     * @param cod_forum the cod_forum to set
+     */
+    public void setCod_forum(int cod_forum) {
+        this.cod_forum = cod_forum;
+    }
+
+    /**
+     * @return the cod_postagem
+     */
+    public int getCod_postagem() {
+        return cod_postagem;
+    }
+
+    /**
+     * @param cod_postagem the cod_postagem to set
+     */
+    public void setCod_postagem(int cod_postagem) {
+        this.cod_postagem = cod_postagem;
+    }
+
+    /**
+     * @return the cod_usuario
+     */
+    public int getCod_usuario() {
+        return cod_usuario;
+    }
+
+    /**
+     * @param cod_usuario the cod_usuario to set
+     */
+    public void setCod_usuario(int cod_usuario) {
+        this.cod_usuario = cod_usuario;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the assunto
+     */
+    public String getAssunto() {
+        return assunto;
+    }
+
+    /**
+     * @param assunto the assunto to set
+     */
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+    private int cod_forum;
+    private int cod_postagem;
+    private int cod_usuario;
+    private String titulo;
+    private String assunto;
 }
